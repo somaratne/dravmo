96b8ddbc2618f133bc03ad05839c7055d234bf49
somaratne <eranga_c@yahoo.ca>
2020-03-11 18:54:56 +0530

    add react persist and now able to save cartitems in localstorage it will help to hold data and

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
 const middlewares = [logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
